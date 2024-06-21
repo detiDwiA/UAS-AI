29f1c8a05b902d616580b8ad1ed2667fabb9aabd
detiDwiA <165123609+detiDwiA@users.noreply.github.com>
2024-06-21 22:18:38 +0700

    Update project.py

--- project.py
+++ project.py
@@ -1,45 +1,131 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
-from imblearn.over_sampling import SMOTE
 
-# Membaca dataset
-credit_card_data = pd.read_csv('clean_dataset.csv')
 
-# Memeriksa distribusi kelas
-print("Distribusi kelas sebelum SMOTE:")
-print(credit_card_data['Class'].value_counts())
+# In[2]:
 
-# Memisahkan fitur (X) dan target (Y)
-X = credit_card_data.drop(columns='Class', axis=1)
-Y = credit_card_data['Class']
 
-# Menggunakan SMOTE untuk oversampling
-smote = SMOTE(random_state=42)
-X_resampled, Y_resampled = smote.fit_resample(X, Y)
+# loading the dataset to a Pandas DataFrame
+credit_card_data = pd.read_csv(r'creditcard.csv')
 
-# Memeriksa distribusi kelas setelah SMOTE
-print("Distribusi kelas setelah SMOTE:")
-print(Y_resampled.value_counts())
 
-# Membagi data menjadi data latih dan data uji dengan stratify setelah resampling
-X_train, X_test, Y_train, Y_test = train_test_split(X_resampled, Y_resampled, test_size=0.2, stratify=Y_resampled, random_state=42)
+# In[3]:
 
-# Inisialisasi model
-model = LogisticRegression(max_iter=1000)
 
-# Melatih model
-model.fit(X_train, Y_train)
+# first 5 rows of the dataset
+credit_card_data.head()
 
-# Melakukan prediksi pada data uji
-Y_pred = model.predict(X_test)
 
-# Menghitung akurasi
-accuracy = accuracy_score(Y_test, Y_pred)
-print(f"Akurasi: {accuracy}")
+# In[4]:
 
-# Menampilkan beberapa prediksi contoh
-print("Prediksi contoh:")
-for i in range(5):
-    print(f"Prediksi: {Y_pred[i]}, Aktual: {Y_test.values[i]}")
+
+credit_card_data.tail()
+
+
+# In[5]:
+
+
+# dataset informations
+credit_card_data.info()
+
+
+# In[6]:
+
+
+# checking the number of missing values in each column
+credit_card_data.isnull().sum()
+
+
+# In[7]:
+
+
+# distribution of legit transactions & fraudulent transactions
+credit_card_data['Class'].value_counts()
+
+
+# In[8]:
+
+
+# separating the data for analysis
+legit = credit_card_data[credit_card_data.Class == 0]
+fraud = credit_card_data[credit_card_data.Class == 1]
+
+
+# In[9]:
+
+
+print(legit.shape)
+print(fraud.shape)
+
+
+# In[10]:
+
+
+# statistical measures of the data
+legit.Amount.describe()
+
+
+# In[11]:
+
+
+fraud.Amount.describe()
+
+
+# In[12]:
+
+
+# compare the values for both transactions
+credit_card_data.groupby('Class').mean()
+
+
+# In[13]:
+
+
+legit_sample = legit.sample(n=492)
+
+
+# In[14]:
+
+
+new_dataset = pd.concat([legit_sample, fraud], axis=0)
+
+
+# In[15]:
+
+
+new_dataset.head()
+
+
+# In[16]:
+
+
+new_dataset.tail()
+
+
+# In[17]:
+
+
+new_dataset['Class'].value_counts()
+
+
+# In[18]:
+
+
+new_dataset.groupby('Class').mean()
+
+
+# In[19]:
+
+
+X = new_dataset.drop(columns='Class', axis=1)
+Y = new_dataset['Class']
+
