fa808d4c9dda4e1c17d7b857fca2b7331c4a4fa0
detiDwiA <165123609+detiDwiA@users.noreply.github.com>
2024-06-22 06:00:44 +0700

    Update project.py

--- project.py
+++ project.py
@@ -1,208 +1,95 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
-
-
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
-
-# In[5]:
-
-
 # loading the dataset to a Pandas DataFrame
-credit_card_data = pd.read_csv(f'dataset.csv')
-
-
-# In[6]:
-
+credit_card_data = pd.read_csv('dataset.csv')
 
 # first 5 rows of the dataset
-credit_card_data.head()
+print(credit_card_data.head())
 
-
-# In[7]:
-
-
-credit_card_data.tail()
-
-
-# In[8]:
-
+# last 5 rows of the dataset
+print(credit_card_data.tail())
 
 # dataset informations
 credit_card_data.info()
 
-
-# In[9]:
-
-
 # checking the number of missing values in each column
-credit_card_data.isnull().sum()
-
-
-# In[10]:
-
+print(credit_card_data.isnull().sum())
 
 # distribution of legit transactions & fraudulent transactions
-credit_card_data['Class'].value_counts()
-
-
-# In[11]:
-
+print(credit_card_data['Class'].value_counts())
 
 # separating the data for analysis
 legit = credit_card_data[credit_card_data.Class == 0]
 fraud = credit_card_data[credit_card_data.Class == 1]
 
-
-# In[12]:
-
-
 print(legit.shape)
 print(fraud.shape)
 
-
-# In[13]:
-
-
 # statistical measures of the data
-legit.Amount.describe()
-
-
-# In[14]:
-
-
-fraud.Amount.describe()
-
-
-# In[15]:
-
+print(legit.Amount.describe())
+print(fraud.Amount.describe())
 
 # compare the values for both transactions
-credit_card_data.groupby('Class').mean()
-
-
-# In[16]:
-
+print(credit_card_data.groupby('Class').mean())
 
 legit_sample = legit.sample(n=492)
 
-
-# In[17]:
-
-
 new_dataset = pd.concat([legit_sample, fraud], axis=0)
 
+print(new_dataset.head())
+print(new_dataset.tail())
 
-# In[18]:
-
-
-new_dataset.head()
-
-
-# In[19]:
-
-
-new_dataset.tail()
-
-
-# In[20]:
-
-
-new_dataset['Class'].value_counts()
-
-
-# In[21]:
-
-
-new_dataset.groupby('Class').mean()
-
-
-# In[22]:
-
+print(new_dataset['Class'].value_counts())
+print(new_dataset.groupby('Class').mean())
 
 X = new_dataset.drop(columns='Class', axis=1)
 Y = new_dataset['Class']
 
-
-# In[23]:
-
-
 print(X)
-
-
-# In[24]:
-
-
 print(Y)
-
-
-# In[27]:
-
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, stratify=Y, random_state=2)
 
-
-# In[28]:
-
-
 print(X.shape, X_train.shape, X_test.shape)
-
-
-# In[29]:
-
 
 model = LogisticRegression(max_iter=1000)
 
-
-# In[37]:
-
-
 # training the Logistic Regression Model with Training Data
 model.fit(X_train, Y_train)
-
-
-# In[31]:
-
 
 # accuracy on training data
 X_train_prediction = model.predict(X_train)
 training_data_accuracy = accuracy_score(X_train_prediction, Y_train)
 
-
-# In[32]:
-
-
 print('Accuracy on Training data : ', training_data_accuracy)
-
-
-# In[33]:
-
 
 # accuracy on test data
 X_test_prediction = model.predict(X_test)
 test_data_accuracy = accuracy_score(X_test_prediction, Y_test)
 
-
-# In[34]:
-
-
 print('Accuracy score on Test Data : ', test_data_accuracy)
 
+# Fungsi untuk memprediksi apakah transaksi baru adalah penipuan atau tidak
+def predict_transaction(model, transaction):
+    prediction = model.predict(transaction)
+    if prediction == 0:
+        print("Transaksi ini sah.")
+    else:
+        print("Transaksi ini penipuan.")
 
-# Contoh data transaksi baru (fitur harus dalam format yang sama dengan data pelatihan)
-new_transaction = np.array([[...]])  # Masukkan nilai fitur transaksi baru di sini
+# Contoh data transaksi baru
+# Gantilah nilai-nilai berikut dengan nilai fitur dari transaksi baru yang ingin Anda prediksi
+new_transaction = np.array([[...]])
 
-# Melakukan prediksi
-prediction = model.predict(new_transaction)
-if prediction == 0:
-    print("Transaksi ini sah.")
-else:
-    print("Transaksi ini penipuan.")
+# Pastikan data transaksi baru memiliki bentuk yang sama dengan data pelatihan (misalnya, jumlah fitur yang sama)
+# Jika perlu, gunakan scaler yang sama dengan yang digunakan pada data pelatihan
 
-
+# Melakukan prediksi pada transaksi baru
+predict_transaction(model, new_transaction)
