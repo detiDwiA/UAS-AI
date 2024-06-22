5c9d8c9259db651db05cef98594619b20b8424bc
detiDwiA <165123609+detiDwiA@users.noreply.github.com>
2024-06-22 07:23:14 +0700

    Update project.py

--- project.py
+++ project.py
@@ -1,196 +1,74 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
+import streamlit as st
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
-
-# In[5]:
-
-
-# loading the dataset to a Pandas DataFrame
+# Load the dataset
 credit_card_data = pd.read_csv(r'dataset.csv')
 
+# Display dataset info in Streamlit app
+st.write("### Dataset Information")
+st.write(credit_card_data.info())
 
-# In[6]:
+# Display first few rows of the dataset
+st.write("### First 5 Rows of the Dataset")
+st.write(credit_card_data.head())
 
+# Display the number of missing values in each column
+st.write("### Missing Values in Each Column")
+st.write(credit_card_data.isnull().sum())
 
-# first 5 rows of the dataset
-credit_card_data.head()
+# Distribution of legit transactions & fraudulent transactions
+st.write("### Distribution of Legit & Fraudulent Transactions")
+st.write(credit_card_data['Class'].value_counts())
 
-
-# In[7]:
-
-
-credit_card_data.tail()
-
-
-# In[8]:
-
-
-# dataset informations
-credit_card_data.info()
-
-
-# In[9]:
-
-
-# checking the number of missing values in each column
-credit_card_data.isnull().sum()
-
-
-# In[10]:
-
-
-# distribution of legit transactions & fraudulent transactions
-credit_card_data['Class'].value_counts()
-
-
-# In[11]:
-
-
-# separating the data for analysis
+# Data preprocessing
 legit = credit_card_data[credit_card_data.Class == 0]
 fraud = credit_card_data[credit_card_data.Class == 1]
 
-
-# In[12]:
-
-
-print(legit.shape)
-print(fraud.shape)
-
-
-# In[13]:
-
-
-# statistical measures of the data
-legit.Amount.describe()
-
-
-# In[14]:
-
-
-fraud.Amount.describe()
-
-
-# In[15]:
-
-
-# compare the values for both transactions
-credit_card_data.groupby('Class').mean()
-
-
-# In[16]:
-
-
 legit_sample = legit.sample(n=492)
-
-
-# In[17]:
-
-
 new_dataset = pd.concat([legit_sample, fraud], axis=0)
 
-
-# In[18]:
-
-
-new_dataset.head()
-
-
-# In[19]:
-
-
-new_dataset.tail()
-
-
-# In[20]:
-
-
-new_dataset['Class'].value_counts()
-
-
-# In[21]:
-
-
-new_dataset.groupby('Class').mean()
-
-
-# In[22]:
-
-
+# Splitting the data into features and targets
 X = new_dataset.drop(columns='Class', axis=1)
 Y = new_dataset['Class']
 
-
-# In[23]:
-
-
-print(X)
-
-
-# In[24]:
-
-
-print(Y)
-
-
-# In[27]:
-
-
+# Splitting the data into training and testing sets
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, stratify=Y, random_state=2)
 
-
-# In[28]:
-
-
-print(X.shape, X_train.shape, X_test.shape)
-
-
-# In[29]:
-
-
+# Training the model
 model = LogisticRegression()
-
-
-# In[37]:
-
-
-# training the Logistic Regression Model with Training Data
 model.fit(X_train, Y_train)
 
+# Streamlit UI
+st.title("Credit Card Fraud Detection")
 
-# In[31]:
+st.write("Enter the transaction details below to predict if it's fraudulent or not.")
 
+# Create input fields for each feature
+input_data = []
+for column in X.columns:
+    value = st.number_input(f"Enter {column}", format="%.2f")
+    input_data.append(value)
 
-# accuracy on training data
+# Convert input data to numpy array
+input_data = np.array(input_data).reshape(1, -1)
+
+# Prediction
+if st.button('Predict'):
+    prediction = model.predict(input_data)
+    if prediction[0] == 0:
+        st.success("The transaction is legitimate.")
+    else:
+        st.error("The transaction is fraudulent.")
+
+# Show the accuracy of the model
 X_train_prediction = model.predict(X_train)
 training_data_accuracy = accuracy_score(X_train_prediction, Y_train)
-
-
-# In[32]:
-
-
-print('Accuracy on Training data : ', training_data_accuracy)
-
-
-# In[33]:
-
-
-# accuracy on test data
 X_test_prediction = model.predict(X_test)
 test_data_accuracy = accuracy_score(X_test_prediction, Y_test)
 
-
-# In[34]:
-
-
-print('Accuracy score on Test Data : ', test_data_accuracy)
-
+st.write(f"Accuracy on Training data: {training_data_accuracy * 100:.2f}%")
+st.write(f"Accuracy on Test data: {test_data_accuracy * 100:.2f}%")
