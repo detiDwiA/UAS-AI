3e4ce78ddf5ec6a3444023e9ec74ef6702dd1279
detiDwiA <165123609+detiDwiA@users.noreply.github.com>
2024-06-22 07:39:28 +0700

    Update project.py

--- project.py
+++ project.py
@@ -9,19 +9,19 @@ from sklearn.metrics import accuracy_score
 credit_card_data = pd.read_csv(r'dataset.csv')
 
 # Display dataset info in Streamlit app
-st.write("### Dataset Information")
+st.write("Informasi dataset")
 st.write(credit_card_data.info())
 
 # Display first few rows of the dataset
-st.write("### First 5 Rows of the Dataset")
+st.write("5 data teratas")
 st.write(credit_card_data.head())
 
 # Display the number of missing values in each column
-st.write("### Missing Values in Each Column")
+st.write("data yang tidak memiliki value di dalam kolom")
 st.write(credit_card_data.isnull().sum())
 
 # Distribution of legit transactions & fraudulent transactions
-st.write("### Distribution of Legit & Fraudulent Transactions")
+st.write("palsu atau asli transaksi")
 st.write(credit_card_data['Class'].value_counts())
 
 # Data preprocessing
@@ -43,21 +43,21 @@ model = LogisticRegression()
 model.fit(X_train, Y_train)
 
 # Streamlit UI
-st.title("Credit Card Fraud Detection")
+st.title("Pendeteksi kepalsuan kartu kredit")
 
-st.write("Enter the transaction details below to predict if it's fraudulent or not.")
+st.write("Masukan data yang ingin di prediksi.")
 
 # Create input fields for each feature
 input_data = []
 for column in X.columns:
-    value = st.number_input(f"Enter {column}", format="%.2f")
+    value = st.number_input(f"masukkan {column}", format="%.2f")
     input_data.append(value)
 
 # Convert input data to numpy array
 input_data = np.array(input_data).reshape(1, -1)
 
 # Prediction
-if st.button('Predict'):
+if st.button('Prediksi sekarang'):
     prediction = model.predict(input_data)
     if prediction[0] == 0:
         st.success("Kartu Kredit Asli")
@@ -70,5 +70,5 @@ training_data_accuracy = accuracy_score(X_train_prediction, Y_train)
 X_test_prediction = model.predict(X_test)
 test_data_accuracy = accuracy_score(X_test_prediction, Y_test)
 
-st.write(f"Accuracy on Training data: {training_data_accuracy * 100:.2f}%")
-st.write(f"Accuracy on Test data: {test_data_accuracy * 100:.2f}%")
+st.write(f"tingkat akurasi data latih: {training_data_accuracy * 100:.2f}%")
+st.write(f"tingkat akurasi test data: {test_data_accuracy * 100:.2f}%")
